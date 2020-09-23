8208889891678702983fedf4f78ce2abf6488011
solofo <soloforahamefy@gmail.com>
2020-09-23 08:20:23 +0300

    comparaison perfs

--- MixSimulator.py
+++ MixSimulator.py
@@ -223,5 +223,73 @@ class MixSimulator:
         plt.yticks(values / value_increment, ['%d' % val for val in values])
         plt.xticks([])
         plt.title('Optimum and Current values')
+
+
+        plt.show()
+
+
+
+        columns=[]
+        tmp=[]
+        data=[]
+        for keys, values in optimum.items():
+            if keys != "usage_coefficient":
+                columns.append(keys)
+                tmp.append(values)
+        data.append(tmp)
+        tmp = []
+        for keys, values in current.items():
+            if keys != "usage_coefficient":
+                tmp.append(values)
+        data.append(tmp)
+                
+        rows = ["current","optimum"]        
+              
+        # Get some pastel shades for the colors
+        colors = plt.cm.autumn(np.linspace(0, 0.5, len(rows)))
+        n_rows = len(data)
+        
+        index = np.arange(len(columns)) + 0.3
+        bar_width = 0.4
+
+        # Initialize the vertical-offset for the stacked bar chart.
+        y_offset = np.zeros(len(columns))
+        
+        # Plot bars and create text labels for the table
+        cell_text = []
+        correction = np.zeros(len(columns))
+        for row in range(n_rows):
+            for i in range(0,len(columns)):
+                if y_offset[i] < data[row][i]:
+                    correction[i] = y_offset[i]
+                    color_correction = row-1
+            y_offset = np.zeros(len(columns))
+            plt.bar(index, data[row], bar_width, bottom=y_offset, color=colors[row])
+            y_offset = data[row]
+            cell_text.append(['%f' % x for x in y_offset])
+            
+        #correction to avoid hidden bar
+        y_offset = np.zeros(len(columns))
+        plt.bar(index, correction, bar_width, bottom=y_offset, color=colors[color_correction])            
+        
+        # Reverse colors and text labels to display the last value at the top.
+        colors = colors[::-1]
+        cell_text.reverse()
+        
+        # Add a table at the bottom of the axes
+        the_table = plt.table(cellText=cell_text,
+                              rowLabels=rows,
+                              rowColours=colors,
+                              colLabels=columns,
+                              loc='bottom')
+        
+        # Adjust layout to make room for the table:
+        plt.subplots_adjust(left=0.2, bottom=0.2)
+        values = np.arange(0, 120, 20)
+        value_increment = 100 
+        plt.ylabel("Performance comparaison")
+        plt.yticks(values / value_increment, ['%d' % val for val in values])
+        plt.xticks([])
+        plt.title('Optimum and Current values')
         
         plt.show()
