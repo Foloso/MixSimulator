c38beae7f5f6eac17277c3f7ef7e0f2ffd6d3256
Toky Axel <tokyaxel@localsite.mg>
2020-09-28 12:20:03 +0300

    evaluation subplot

--- Evaluation.py
+++ Evaluation.py
@@ -11,8 +11,9 @@ class Evaluation:
         tmp = Optimizer()
         self.__available_optimizers = tmp.getOptimizerList()
 
-    def plot_evaluation(self, X = 0,Y = 0,label : list = ["Optimizer"],label_y : str = "values",max_budgets = 0):
+    def plot_evaluation(self, X, Y, label_y : List['str'], label : List = ["Optimizer"], max_budgets = 0):
 
+        """
         # Y units
         N=X[0]
         if N>=1000000: 
@@ -25,31 +26,32 @@ class Evaluation:
             else :
                 units=1
                 labelY=label_y
+        """
         
-        #init
-        fig = plt.figure(facecolor='w')
-        ax = fig.add_subplot(111,axisbelow=True)
+        #init subplot
+        fig, axs = plt.subplots(1, len(label_y), figsize=(12, 6))        
         
-        # data integration
-        for i in range(0,len(Y)):            
-            ax.plot(X, Y[i]/units, alpha=0.5, lw=2, label=label[i])
-            #ax.plot(max_budgets, I/units, 'r', alpha=0.5, lw=2, label='Infected')
-            #ax.plot(max_budgets, R/units, 'g', alpha=0.5, lw=2, label='Recovered with immunity')
+        # data integration        
+        for n_axs in range(0,len(axs)) :
+            dict_ = Y[label_y[n_axs]]
+            for opt, value in dict_.items():
+                axs[n_axs].plot(X, value, alpha=0.5, lw=2, label=str(opt))
         
         # labels and parametrizations    
-        ax.set_xlabel('Budgets')
-        ax.set_ylabel(labelY)
-        ax.set_ylim(0,np.amax(Y[0])+300)
-        ax.yaxis.set_tick_params(length=0)
-        ax.xaxis.set_tick_params(length=0)
-        ax.grid(b=True, which='major', c='w', lw=2, ls='-')
-        legend = ax.legend()
-        legend.get_frame().set_alpha(0.5)
-        for spine in ('top', 'right', 'bottom', 'left'):
-            ax.spines[spine].set_visible(False)
+
+        #axs.grid(b=True, which='major', c='w', lw=2, ls='-')
+        for n_axs in range(0,len(axs)) :
+            axs[n_axs].yaxis.set_tick_params(length=0)
+            axs[n_axs].xaxis.set_tick_params(length=0)
+            axs[n_axs].set_xlabel('Budgets')
+            axs[n_axs].set_ylabel(label_y[n_axs])
+            axs[n_axs].legend()
+        #legend.get_frame().set_alpha(0.5)
+        #for spine in ('top', 'right', 'bottom', 'left'):
+        #    axs.spines[spine].set_visible(False)
         plt.show()
         
-    def evaluate(self, mix, sequence, max_budgets, optimizer_list: List['str'] ,bind=None) :        
+    def evaluate(self, mix, sequence, max_budgets, optimizer_list: List['str'], indicator_list: List['str'], bind=None, carbonProdLimit: float = 39500000000, time_interval : float = 2) :        
         #setting dataset
         if bind != None:
             mix.set_data_csv(str(bind))
@@ -64,18 +66,42 @@ class Evaluation:
             return
 
         #process
+        y_tmp = {}
         y=[]
         budget = np.arange(0, max_budgets, sequence)
+        prod_per_opt={}
+        carbon_per_opt={}
+        demand_per_opt={}
         for opt in optimizer_list:
-            y_tmp = [] 
+            prod_per_budget=[]
+            carbon_per_budget=[]
+            demand_per_budget=[]
             for b in budget:
-                data=mix.simuleMix(current_usage_coef=[0.6, 0.2, 0.7, 0.95], carbonProdLimit= 3950000000000,
-                            time_interval = 2, optimize_with = [opt], budgets = [b], plot = "none", verbose = 1)
-                y_tmp.append(float(data["production_cost ($)"]))
-                #X.append(float(b+10))
-            #plotting
-            y.append(np.array(y_tmp))
-        self.plot_evaluation(X=np.array(budget),Y=y,label=optimizer_list, label_y = "production_cost ($)", max_budgets = max_budgets)
+                data=mix.simuleMix(current_usage_coef=[0.6, 0.2, 0.7, 0.95], carbonProdLimit= carbonProdLimit, 
+                                   time_interval = time_interval, optimize_with = [opt], budgets = [b], plot = "none", verbose = 1)
+                #for indicator in indicator_list:
+                #   y_tmp.update({indicator:float(data[indicator])})
+                prod_per_budget.append(float(data["production_cost ($)"]))
+                carbon_per_budget.append(float(data["carbon_impacte (g/MWh)"]))
+                demand_per_budget.append(float(data["unsatisfied_demand (MWh)"]))
+            prod_per_opt.update({opt:prod_per_budget})
+            carbon_per_opt.update({opt:carbon_per_budget})
+            demand_per_opt.update({opt:demand_per_budget})
+        y_tmp.update({"production_cost ($)":prod_per_opt})
+        y_tmp.update({"carbon_impacte (g/MWh)":carbon_per_opt})
+        y_tmp.update({"unsatisfied_demand (MWh)":demand_per_opt})
         
+        #plotting
+
+#        y_re_tmp = {}
+#        for indicator in indicator_list:
+#            list_array = []
+#            for cur_y in y:
+#                print(cur_y)
+#                list_array.append(cur_y[indicator])
+#            y_re_tmp.update({indicator: list_array})
+#        print(y_re_tmp)
+        self.plot_evaluation(X=np.array(budget),Y=y_tmp,label_y = indicator_list, label=optimizer_list, max_budgets = max_budgets)
+        #self, X = 0,Y = 0,label : list = ["Optimizer"],label_y : List["str"], max_budgets = 0
     ########EXAMPLES    
     # evaluate(sys.argv[1], 10, 160)
