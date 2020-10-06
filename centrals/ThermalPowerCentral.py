89fa695879fd428af1466300c8f039ca4e32c175
solofo <soloforahamefy@gmail.com>
2020-10-06 18:57:21 +0300

    minor update

--- centrals/ThermalPowerCentral.py
+++
@@ -1,8 +0,0 @@
-from .centrals import PowerCentral as pc
-
-class ThermalPowerCentral(pc):
-    """
-        Class of power plant with the specifications of a Thermal Power Plant
-    """
-    def __init__(self):
-        self.__tuneable = True
